c232b793204bff40125fecddbfad8b88dbe9e234
ChiSengWong <95706415+ChiSengWong@users.noreply.github.com>
2022-04-24 17:48:57 -0700

    changed at line 20 - added condition to break loop when any brackets or parenthesis is not found to prevent infinite loop

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,7 +16,8 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if (openBracket == -1) break; // if next openBracket is not found, break the loop to prevent infinite loop.
+            // if next any brackets or parenthesis is not found, break the loop to prevent infinite loop.
+            if (openBracket == -1 || closeBracket == -1 || openParen == -1 || closeParen == -1) break;
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
